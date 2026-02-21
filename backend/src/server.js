2fc23c8d3c54445991cc28501db2a273ee2660bf
minhquang1610 <quangnguyen2005gt08@gmail.com>
2026-02-21 10:27:48 +0700

    Database connection setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import connectDB from "./libs/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// middlewares
 app.use(express.json());
-app.listen(PORT, () => {
-  console.log("Server is listening on port:", PORT);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is listening on port:", PORT);
+  });
 });
